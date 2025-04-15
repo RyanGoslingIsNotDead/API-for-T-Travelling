0980eaf9556f8c713b416aeae57a225e0aaab54d
Кадим <arslanov.kadim@gmail.com>
2025-04-15 22:20:22 +0300

    Initial commit

---
+++ src/main/java/ru/itis/api/ApiForTTravellingApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForTTravellingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiForTTravellingApplication.class, args);
+    }
+
+}
